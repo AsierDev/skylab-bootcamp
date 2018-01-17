9ef84ca61aaecbc380fa97f7a0e17f11677fb73c
AsierDev <asierp.dev@gmail.com>
2018-01-17 19:45:53 +0100

    finish prototypes

--- course/topics/javascript/prototypes/scripts/main.js
+++ course/topics/javascript/prototypes/scripts/main.js
@@ -26,8 +26,8 @@ Person.prototype.bio = function () {
         return this.firstName + " " + this.lastName + " is " + this.age + " years old " + "she likes " + this.interests;
     }
 }
-var f = new Person("Maria", "Lopez", 29, "f", ["internet", "travel", "singing", "reading"]);
-var m = new Person("Pedro", "Gomez", 19, "m", ["games", "futbol", "beer", "films"]);
+var woman = new Person("Maria", "Lopez", 29, "f", ["internet", "travel", "singing", "reading"]);
+var man = new Person("Pedro", "Gomez", 19, "m", ["games", "futbol", "beer", "films"]);
 
 /* Student
 
@@ -38,10 +38,33 @@ b) The Student must inherit all methods from Person and overwrite greeting metho
 function Student(firstName, lastName, age, gender, interests) {
     Person.call(this, firstName, lastName, age, gender, interests)
 }
-Student.prototype = new Person();
+// Student.prototype = new Person();
 
 Student.prototype.greeting = function() {
     return "Yo! I'm " +  this.firstName;
 }
 
-var z = new Student("Paco", "Gomez", 119, "m", ["games", "futbol", "beer", "films"])
+var student = new Student("Paco", "Gomez", 119, "m", ["games", "futbol", "beer", "films"])
+
+
+/* Teacher
+
+a) Write a constructor function called Teacher that inherit from Person and that has the following arguments:
+b) The Teacher must inherit all methods from Person and overwrite greeting method to change its behavior to print:
+
+*/
+
+function Teacher(firstName, lastName, age, gender, interests, subject) {
+    this.subject = subject;
+    Person.call(this, firstName, lastName, age, gender, interests);
+}
+
+Teacher.prototype.greeting = function () {
+    if (this.gender == "male" || this.gender == "m") {
+        return 'Hello! My name is Mr. '  + this.lastName + ' and I teach ' + this.subject;
+    } else if (this.gender == "female" || this.gender == "f") {
+        return 'Hello! My name is Mrs. ' + this.lastName + ' and I teach ' + this.subject;
+    } 
+}
+var teacher = new Teacher("Natxo", "Gomez", 47, "m", ["games", "futbol", "beer", "films"], "english");
+var teachera = new Teacher("Vic", "Kirkham", 29, "f", ["reading", "teaching", "series"], "english");

89153003858df0b8b3b5ea6c63308a74d2fc0df2
AsierDev <asierp.dev@gmail.com>
2018-02-20 10:39:41 +0100

    add nodejs exercise

---
+++ course/backend/nodejs/learnyounode/calculator.js
@@ -0,0 +1,13 @@
+switch (sum) {
+    case 'sum':
+        res = a + b
+        break
+    case 'mult':
+        res = a * b
+        break
+    case 'div':
+        res = a / b
+        break
+}
+
+console.log(res)
---
+++ course/backend/nodejs/learnyounode/exercise3.js
@@ -0,0 +1,7 @@
+
+let str = buf.toString() 
+
+let strArr = str.split('\n')
+
+console.log(strArr.length -1)
+
